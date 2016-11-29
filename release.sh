2d67882d01e76d0cdf7c4666cf4cc4340db5b885
Oleg Zaytsev <oleg@zaytsev.es>
2016-11-30 00:09:49 +0100

    added release script

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+datetag=$(date +%Y%m%d-%H%M%S)
+changeset=$(git rev-parse HEAD)
+releasename="release-$datetag-$changeset"
+docker run -it -w /libphonenumber-info -v $(pwd):/libphonenumber-info node npm run build
+cp -r build $releasename
+ln -sf $releasename release
