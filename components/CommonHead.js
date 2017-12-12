4bf828e95451c506732c616e5eee6a87f0e21f5e
Oleg Zaytsev <oleg@zaytsev.es>
2017-12-12 22:23:24 +0100

    Some minimal SEO

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -6,10 +6,12 @@ export default() =>
     <meta charset="utf-8"></meta>
     <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 
+    <meta name="description" content="Information about phone numbers provided by Google's libphonenumber library." />
+
     <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet"></link>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"></link>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css"></link>
-    <title>libphonenumber.info</title>
+    <title>libphonenumber.info - phone numbers validation and information</title>
   </Head>
